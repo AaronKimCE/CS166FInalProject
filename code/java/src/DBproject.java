7ae02a954de0d0f584e5c360a51a67d08b6b1f61
Aaron Kim <32529743+AaronKimCE@users.noreply.github.com>
2021-06-10 21:08:50 -0700

    Part 7 Fix 3

--- code/java/src/DBproject.java
+++ code/java/src/DBproject.java
@@ -854,10 +854,7 @@ public class DBproject{
 		rs3.add(rs3b);
 
 		try { // Run the query
-			String query = "SELECT D.doctor_ID, COUNT(A1.appnt_ID), COUNT(A2.appnt_ID), COUNT(A3.appnt_ID), COUNT(A4.appnt_ID) 
-			                FROM Doctor D, has_appointment H, Appointment A
-							WHERE D.doctor_ID = H.doctor_ID AND H.appt_ID = A1.appnt_ID AND H.appt_ID = A2.appnt_ID AND H.appt_ID = A3.appnt_ID AND H.appt_ID = A4.appnt_ID AND 
-							A1.status = \'PA\' AND A2.status = \'AC\' AND A3.status = \'AV\' AND A4.status = \'WL\' GROUP BY D.doctor_ID ORDER BY D.doctor_ID ASC;";
+			String query = "SELECT D.doctor_ID, COUNT(A1.appnt_ID), COUNT(A2.appnt_ID), COUNT(A3.appnt_ID), COUNT(A4.appnt_ID) FROM Doctor D, has_appointment H, Appointment A WHERE D.doctor_ID = H.doctor_ID AND H.appt_ID = A1.appnt_ID AND H.appt_ID = A2.appnt_ID AND H.appt_ID = A3.appnt_ID AND H.appt_ID = A4.appnt_ID AND A1.status = \'PA\' AND A2.status = \'AC\' AND A3.status = \'AV\' AND A4.status = \'WL\' GROUP BY D.doctor_ID ORDER BY D.doctor_ID ASC;";
 			esql.executeQueryAndPrintResult(query);
 		} catch (Exception e) {
 			System.out.println("Table Search Error!!");
